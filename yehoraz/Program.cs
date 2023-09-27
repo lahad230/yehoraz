c441f1de6e3361d7d1de584fbea9fd8e75640915
lahad230 <lahad230@gmail.com>
2023-09-27 14:06:36 +0300

    yehoraz

---
+++ yehoraz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace yehoraz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonConvert.DefaultSettings = JsonConvert.DefaultSettings;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
